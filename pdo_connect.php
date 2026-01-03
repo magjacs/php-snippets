8c69a8cbd840e22393aec60681172e9ec78bf8a2
magjacs <magjacs@mail.ru>
2026-01-04 00:15:35 +0500

    Update pdo_connect.php

--- pdo_connect.php
+++ pdo_connect.php
@@ -1,3 +1,10 @@
 //Подключение К БД
 
 $pdo = new PDO("mysql:host=localhost;dbname=crud;","root","");
+
+
+Создание записи CRUD
+
+$sql = "INSERT INTO products (title, description, price) VALUES (:title, :description, :price)";
+$statement = $pdo -> prepare($sql);
+$product = $statement -> execute(["title" => $title,"description" => $description,"price" => $price]);
